e4cdd77193b8bca0804e884bb8bb04bd8817e5b8
Adigotlib <adi.gotlib@post.runi.ac.il>
2024-11-25 21:58:47 +0200

    trying

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,12 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double endBalance = loan;
+		for (int i = 0; i < n; i++ )
+		{
+			endBalance = (endBalance - payment)*(rate + 1);
+		}
+		return endBalance;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +42,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double increment = 0.0001;
+		double g = 0;
+		while (Math.abs(endBalance(loan, rate, n, g)) <= epsilon) 
+		{
+			g += increment;
+			iterationCounter ++;
+		}
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
